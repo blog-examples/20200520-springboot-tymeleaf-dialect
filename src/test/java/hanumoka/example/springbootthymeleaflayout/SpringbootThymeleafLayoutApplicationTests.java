628cf3d9f94363b844ab765462280dd131aa0666
amagramer <amagramer@gmail.com>
2020-05-20 21:47:46 +0900

    first commit

---
+++ src/test/java/hanumoka/example/springbootthymeleaflayout/SpringbootThymeleafLayoutApplicationTests.java
@@ -0,0 +1,13 @@
+package hanumoka.example.springbootthymeleaflayout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootThymeleafLayoutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
